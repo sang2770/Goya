eb547b593852b541b249320fc6d05fbcfec15649
Nguyen van sang <nguyenvansang30102001@gmail.com>
2021-04-20 16:10:26 +0700

    Bo sung Slide

--- ThucHanh2/slider.js
+++ ThucHanh2/slider.js
@@ -8,10 +8,16 @@ var Size=listimg.length-1;
 console.log(Size);
 var count=0;
 var a="banner_active"
-var time=setInterval(()=>{
-    btn_right.click();
-}, 8000)
+var time=0;
+function timer(ms)
+{
+    time=setInterval(()=>{
+        btn_right.click();
+    }, ms)
+}
+timer(5000);
 btn_right.onclick=()=>{  
+    clearInterval(time);
     if(count !=Size)
     {
         
@@ -19,10 +25,10 @@ btn_right.onclick=()=>{
         count++;
         console.log(`${a}`);
         listimg[count].classList.add(`${a}`, "rightToleft");
-        listimg[count].addEventListener("animationend", ()=>{
+        let end=setTimeout(()=>{
             listimg[count].classList.remove("rightToleft");
             listimg[count-1].classList.remove(`${a}`, "toLeft");
-            })
+            },850)
     }
     else
     {
@@ -30,24 +36,26 @@ btn_right.onclick=()=>{
         count=0;
         console.log(`${a}`);
         listimg[0].classList.add(`${a}`, "rightToleft");
-        listimg[0].addEventListener("animationend", ()=>{
+        let end=setTimeout(()=>{
             listimg[0].classList.remove("rightToleft");
             listimg[Size].classList.remove(`${a}`, "toLeft");
-            })
+            }, 850)
         
     }
+    timer(5000);
 }
 btn_left.onclick=()=>{
+    clearInterval(time);
     if(count !=0)
     {
         listimg[count].classList.add( "toRight");
         count--;
         console.log(`${a}`);
         listimg[count].classList.add(`${a}`, "leftToright");
-        listimg[count].addEventListener("animationend", ()=>{
+        let end=setTimeout(()=>{
             listimg[count].classList.remove("leftToright");
             listimg[count+1].classList.remove(`${a}`, "toRight");
-            })
+            },850)
     }
     else
     {
@@ -55,12 +63,13 @@ btn_left.onclick=()=>{
         count=Size;
         console.log(`${a}`);
         listimg[count].classList.add(`${a}`, "leftToright");
-        listimg[count].addEventListener("animationend", ()=>{
+        let end=setTimeout(()=>{
             listimg[count].classList.remove("leftToright");
             listimg[0].classList.remove(`${a}`, "toRight");
-            })
+            },850)
         
     }
+    timer(5000);
 }
 var test=new Promise((resolve)=>{
     resolve("ok");
